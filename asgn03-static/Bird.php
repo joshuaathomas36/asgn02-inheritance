ed02e3d4ead2e1b771b7092c12cde7b210d1d2e5
Joshua Thomas <joshuaathomas36@students.abtech.edu>
2021-09-21 11:02:24 -0400

    asgn03 changes for resubmiting complete

--- asgn03-static/Bird.php
+++ asgn03-static/Bird.php
@@ -1,12 +1,12 @@
 <?php
 
 class Bird {
-    var $habitat;
-    var $food;
-    var $nesting = "tree";
-    var $conservation;
-    var $song = "chirp";
-    var $flying = "yes";
+    public static $habitat;
+    public static $food;
+    public static $nesting = "tree";
+    public static $conservation;
+    public static $song = "chirp";
+    public static $flying = true;
     public static $instatnce_count = 0;
     public static $egg_num = 0;
 
@@ -17,21 +17,20 @@ class Bird {
         return $obj;
     }
 
-    function can_fly() {
-        $flying_string = ($this->flying == "yes") ? "can fly" : "is stuck on the ground";
-        return  $flying_string;
+    public static function can_fly() {
+        return $flying_string = static::$flying ? "can fly" : "is stuck on the ground";
     }
 }
 
 class YellowBelliedFlyCatcher extends Bird {
-    var $name = "yellow-bellied flycatcher";
-    var $diet = "mostly insects.";
-    var $song = "flat chilk";
+    public static $name = "yellow-bellied flycatcher";
+    public static $diet = "mostly insects.";
+    public static $song = "flat chilk";
     public static $egg_num = "3-4, sometimes 5";
 }
 
 class Kiwi extends Bird {
-    var $name = "kiwi";
-    var $diet = "omnivorous";
-    var $flying = "no";
+    public static $name = "kiwi";
+    public static $diet = "omnivorous";
+    public static $flying = false;
 }
--- asgn03-static/index.php
+++ asgn03-static/index.php
@@ -12,15 +12,15 @@
     include 'Bird.php';
     
     $bird = new Bird;
-    echo '<p>The generic song of any bird is "' . $bird->song . '".</p>';
+    echo '<p>The generic song of any bird is "' . Bird::$song . '".</p>';
 
     $fly_catcher = new YellowBelliedFlyCatcher;
-    echo '<p>The song of the ' . $fly_catcher->name . ' on breeding grounds is "' . $fly_catcher->song . '".</p>';
+    echo '<p>The song of the ' . $fly_catcher::$name . ' on breeding grounds is "' . $fly_catcher::$song . '".</p>';
 
     $kiwi = new Kiwi;
-    $kiwi->flying = "no";
-    echo "<p>The " . $fly_catcher->name . " " . $fly_catcher->can_fly() . ".</p>";
-    echo "<p>The " . $kiwi->name . " " . $kiwi->can_fly() . ".</p> <hr />";   
+    $kiwi::$flying = false;
+    echo "<p>The " . $fly_catcher::$name . " " . $fly_catcher::can_fly() . ".</p>";
+    echo "<p>The " . $kiwi::$name . " " . $kiwi::can_fly() . ".</p> <hr />";   
     
     echo "<h1>Static Examples</h1>";
     echo "<h2>Before using the create method</h2>";
